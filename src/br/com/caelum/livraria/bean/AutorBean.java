bdebf651b731dba03fe7b660b5c48a7d731b207f
Albertino Neto <albeneto72@gmail.com>
2020-04-11 22:54:27 -0300

    Aplicando temas do primefaces

--- src/br/com/caelum/livraria/bean/AutorBean.java
+++ src/br/com/caelum/livraria/bean/AutorBean.java
@@ -1,12 +1,16 @@
 package br.com.caelum.livraria.bean;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
+import javax.persistence.EntityManager;
 
 import br.com.caelum.livraria.dao.DAO;
+import br.com.caelum.livraria.dao.JPAUtil;
 import br.com.caelum.livraria.modelo.Autor;
+import br.com.caelum.livraria.modelo.Livro;
 
 @ManagedBean
 @ViewScoped
@@ -56,9 +60,11 @@ public class AutorBean {
 	public void carregar(Autor autor) {
 		this.autor = autor;
 	}
-
+		
 	public void remover(Autor autor) {
-		new DAO<Autor>(Autor.class).remove(autor);
+		if(1==2) {
+			new DAO<Autor>(Autor.class).remove(autor);
+		}
 	}
 
 }
